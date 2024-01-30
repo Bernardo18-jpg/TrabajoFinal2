f30929073667186a7559f21ee097b62203f8e8f0
Bernardo <goanebernardo18@gmail.com>
2024-01-30 00:59:11 -0300

    actualizando contrasenas

--- Admin.cpp
+++ Admin.cpp
@@ -18,6 +18,7 @@ struct Usuarios
 void profesionales(FILE *Prof, Profesionales pro);
 void usuarios(FILE *Usua, Usuarios usu);
 bool verifUsuario(char usuar[10], FILE *Usua, Usuarios usu);
+bool verifContrase(char contra[10], FILE *Usua, Usuarios usu);
 
 main()
 {
@@ -37,7 +38,7 @@ main()
         printf("\nError al abrir el archivo Usuarios\n");
         exit(1);
     }
-    //profesionales(Prof, pro);
+    // profesionales(Prof, pro);
     usuarios(Usua, usu);
     fclose(Prof);
 }
@@ -76,27 +77,32 @@ void usuarios(FILE *Usua, Usuarios usu)
 {
     int sino = 0;
     char usuar[10], contra[10];
-    bool u;
+    bool u, c;
     fwrite(&usu, sizeof(Usuarios), 1, Usua);
     do
     {
-        do{
-            _flushall();
-            printf("\n\n\tNuevo Usuario:\n\n");
-            printf("Apellido y Nombre: ");
-            gets(usu.apenom);
+        _flushall();
+        printf("\n\n\tNuevo Usuario:\n\n");
+        printf("Apellido y Nombre: ");
+        gets(usu.apenom);
+        do
+        {
             printf("Usuario: ");
             gets(usuar);
+            u = verifUsuario(usuar, Usua, usu);
+        } while (u == true);
+        do
+        {
             printf("Contrasena: ");
             gets(contra);
-            u = verifUsuario(usuar,Usua,usu);
-        }while (u==true);    
+            c = verifContrase(contra, Usua, usu);
+        } while (c == true);
         strcpy(usu.usuario, usuar);
-
+        strcpy(usu.contrase, contra);
         fwrite(&usu, sizeof(Usuarios), 1, Usua);
         do
         {
-            printf("\nDesea agregar otro Usuario? (1 = Si/0 = No)");
+            printf("\nDesea agregar otro Usuario? (1 = Si/0 = No): ");
             scanf("%d", &sino);
             _flushall();
             if (sino > 1 || sino < 0)
@@ -110,14 +116,14 @@ void usuarios(FILE *Usua, Usuarios usu)
 bool verifUsuario(char usuar[10], FILE *Usua, Usuarios usu)
 {
     bool b = false;
-    int cdigit=0, cmayus=0;
+    int cdigit = 0, cmayus = 0;
     fseek(Usua, sizeof(Usuarios), SEEK_SET);
     fread(&usu, sizeof(Usuarios), 1, Usua);
     while (!feof(Usua))
     {
         if ((strcmp(usu.usuario, usuar) == 0))
         {
-            printf("Usuario incorrecto...YA EXISTE %s\n",usuar);
+            printf("Usuario incorrecto...YA EXISTE %s\n", usuar);
             b = true;
         }
         fread(&usu, sizeof(Usuarios), 1, Usua);
@@ -138,22 +144,26 @@ bool verifUsuario(char usuar[10], FILE *Usua, Usuarios usu)
         b = true;
     }
 
-    for (int i = 1; i < strlen(usuar) ; i++)
+    for (int i = 1; i < strlen(usuar); i++)
     {
-        if (usuar[i]>='0' && usuar[i]<='9'){
+        if (usuar[i] >= '0' && usuar[i] <= '9')
+        {
             cdigit++;
         }
-        if (usuar[i] >='A' && usuar[i]<='Z'){
+        if (usuar[i] >= 'A' && usuar[i] <= 'Z')
+        {
             cmayus++;
         }
     }
-    if (cdigit>3){
+    if (cdigit > 3)
+    {
         printf("Usuario incorrecto...HASTA TRES DIGITOS NUMERICOS\n");
         b = true;
     }
-    if (cmayus<2){
+    if (cmayus < 2)
+    {
         printf("Usuario incorrecto...ALMENOS DOS MAYUSCULAS\n");
-        b=true;
+        b = true;
     }
     return b;
 }
@@ -161,4 +171,65 @@ bool verifUsuario(char usuar[10], FILE *Usua, Usuarios usu)
 bool verifContrase(char contra[10], FILE *Usua, Usuarios usu)
 {
     bool b = false;
-    
+    int cdigit = 0, cmayus = 0, cminus = 0;
+
+    for (int i = 1; i < strlen(contra); i++)
+    {
+        if (contra[i] >= '0' && contra[i] <= '9')
+        {
+            cdigit++;
+            if (contra[i + 1] >= '0' && contra[i + 1] <= '9')
+            {
+                if (contra[i + 2] >= '0' && contra[i + 2] <= '9')
+                {
+                    if (contra[i + 3] >= '0' && contra[i + 3] <= '9')
+                    {
+                        b = true;
+                        printf("Contrasena incorrecta...NO PUEDEN HABER MAS DE 3 NUMEROS CONSECUTIVOS\n");
+                    }
+                }
+            }
+        }
+        if (contra[i-1] >= 'A' && contra[i-1] <= 'Z')
+        {
+            cmayus++;
+        }
+        if (contra[i] >= 'a' && contra[i] <= 'z')
+        {
+            cminus++;
+        }
+        if (contra[i] <= '0' && contra[i] >= 'z')
+        {
+            printf("Contrasena incorrecta...SOLO CARACTERES ALFANUMERICOS\n");
+            b = true;
+        }
+    }
+    if (cdigit < 1)
+    {
+        printf("Contrasena incorrecta...MINIMO UN DIGITO NUMERICO\n");
+        b = true;
+    }
+    if (cmayus < 1)
+    {
+        printf("Contrasena incorrecta...MINIMO UNA MAYUSCULA\n");
+        b = true;
+    }
+    if (cminus < 1)
+    {
+        printf("Contrasena incorrecta...MINIMO UNA MINUSCULA\n");
+        b = true;
+    }
+
+    if (strlen(contra) < 6)
+    {
+        printf("Contrasena incorrecta...DEMASIADO CORTA\n");
+        b = true;
+    }
+    else if (strlen(contra) > 32)
+    {
+        printf("Contrasena incorrecta...DEMASIADO LARGA\n");
+        b = true;
+    }
+
+    return b;
+}
